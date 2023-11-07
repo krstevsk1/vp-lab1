b7b22d9f479fc72ca971d9b40ba73a525eac8de1
krstevsk1 <krstevski400@gmail.com>
2023-11-07 11:43:18 +0100

    v3

--- src/main/java/mk/finki/ukim/mk/lab/web/AuthorDetails.java
+++ src/main/java/mk/finki/ukim/mk/lab/web/AuthorDetails.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import mk.finki.ukim.mk.lab.model.Author;
+import mk.finki.ukim.mk.lab.model.Book;
 import mk.finki.ukim.mk.lab.service.AuthorService;
 import mk.finki.ukim.mk.lab.service.BookService;
 import org.thymeleaf.context.WebContext;
@@ -15,17 +16,15 @@ import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/authors")
-public class newAuthorServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/authorDetails")
+public class AuthorDetails extends HttpServlet {
     private final SpringTemplateEngine springTemplateEngine;
     private final AuthorService authorService;
-    private final BookService bookService;
-
-    public newAuthorServlet(SpringTemplateEngine springTemplateEngine, AuthorService authorService, BookService bookService) {
+    public AuthorDetails(SpringTemplateEngine springTemplateEngine, AuthorService authorService) {
         this.springTemplateEngine = springTemplateEngine;
         this.authorService = authorService;
-        this.bookService = bookService;
     }
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -34,26 +33,15 @@ public class newAuthorServlet extends HttpServlet {
                 .buildExchange(req, resp);
 
         WebContext context =  new WebContext(webExchange);
-        context.setVariable("authors", authorService.listAuthors());
-        context.setVariable("ipAddress", req.getRemoteAddr());
+        Long selectedId = Long.valueOf(req.getParameter("selectedId"));
 
-
-        String selectedIsbn = req.getParameter("selectedIsbn");
-
-        context.setVariable("selectedBookIsbn", selectedIsbn);
+        Author selectedAuthor = authorService.findById(selectedId);
+        context.setVariable("selectedAuthor", selectedAuthor);
 
         springTemplateEngine.process(
-                "listAuthors.html",
+                "authorDetails.html",
                 context,
                 resp.getWriter()
         );
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //String selectedIsbn = req.getParameter("bookIsbn");
-        String authorIdString = req.getParameter("authorId");
-        //Author addedAuthor = bookService.addAuthorToBook(Long.valueOf(authorIdString), selectedIsbn);
-        resp.sendRedirect("/authorDetails?selectedId=" + authorIdString);
     }
 }
